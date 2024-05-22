d3eae44e2503706a56d03f9ac4cb1b364f0f4e8d
HENRY <henryuks129@gmail.com>
2024-05-22 22:20:14 +0100

    feat: added fonts-style

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var navLinks = document.querySelectorAll('.nav_link');
-    var currentPath = window.location.pathname.split('/').pop();
-    navLinks.forEach(function(link) {
-        var linkPath = link.getAttribute('href').split('/').pop();
-        if (linkPath === currentPath) {
-            link.classList.add('active');
-        }
-    });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     var navLinks = document.querySelectorAll('.nav_link');
+//     var currentPath = window.location.pathname.split('/').pop();
+//     navLinks.forEach(function(link) {
+//         var linkPath = link.getAttribute('href').split('/').pop();
+//         if (linkPath === currentPath) {
+//             link.classList.add('active');
+//         }
+//     });
+// });
